e7243f2f9ca558e6f5825e01938c6b7417929ea1
Asad Bukhari <asadbukhari9895@gmail.com>
2023-02-27 20:24:07 +0500

    initail commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/utils/templates/index.html");
+});
+
+io.on("connection", socket => {
+	console.log("a user connected");
+	socket.broadcast.emit("hi");
+
+	socket.on("chat message", msg => {
+		io.emit("chat message", msg);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+});
+
+server.listen(5001, () => {
+	console.log("listening on *:5001");
+});
