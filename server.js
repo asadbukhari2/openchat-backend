50c9165217edbd85da3715931f80dda3738195f6
Asad Bukhari <asadbukhari9895@gmail.com>
2023-02-27 22:57:52 +0500

    first commit message user and typing enabled

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/utils/templates/index.html");
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
 });
 
-io.on("connection", socket => {
-	console.log("a user connected");
-	socket.broadcast.emit("hi");
+app.use(cors());
 
-	socket.on("chat message", msg => {
-		io.emit("chat message", msg);
+app.get("/", (req, res) => {
+	res.send("yha kya kr rhy ho?");
+});
+
+let users = [];
+
+io.on("connection", socket => {
+	console.log(`⚡: ${socket?.id} user just connected!`);
+
+	socket.on("newUser", data => {
+		users.push(data);
+		io.emit("newUserResponse", users);
 	});
 
+	socket.on("message", data => {
+		io.emit("messageResponse", data);
+	});
+
+	socket.on("typing", data => socket.broadcast.emit("typingResponse", data));
+
 	socket.on("disconnect", () => {
-		console.log("user disconnected");
+		console.log("'🔥: A user disconnected'");
+		users = users.filter(user => user.socketID !== socket.id);
+		io.emit("newUserResponse", users);
 	});
 });
 
